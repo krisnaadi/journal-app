dc1e03e4220211629ef71b5919efbdee6ab04477
krisnaadiSuastika <krisnaadi@kbsdigital.co.id>
2024-11-13 10:57:39 +0800

    init

---
+++ database/factories/JournalFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Journal>
+ */
+class JournalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'date' => $this->faker->dateTime(),
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'user_id' => User::factory()
+        ];
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+import ajax from '@imacrayon/alpine-ajax'
+
+window.Alpine = Alpine;
+Alpine.plugin(ajax);
+
+Alpine.start();
