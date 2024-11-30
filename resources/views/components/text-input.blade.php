6316a719d5ce8be239a555fedd967d675b1a66c3
krisnaadi <man.krisna@gmail.com>
2024-11-30 13:09:42 +0800

    fix(input): fix input date style

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'flex w-full h-10 px-3 py-2 text-sm bg-white border rounded-md border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50']) }}/>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'w-full h-10 px-3 py-2 text-sm bg-white border rounded-md border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50']) }}/>
--- resources/views/journals/list.blade.php
+++ resources/views/journals/list.blade.php
@@ -33,9 +33,7 @@
                                 }" x-init="resize()" @input="resize()" type="text"
                                     placeholder="Write your day..."
                                     name="description"
-                                    class="flex w-full h-auto min-h-[100px] px-3 py-2 text-sm bg-white border rounded-md border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50">
-                                    {{ old('description') }}
-                                </textarea>
+                                    class="flex w-full h-auto min-h-[100px] px-3 py-2 text-sm bg-white border rounded-md border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50">{{ old('description') }}</textarea>
                                 @error('date')
                                     <span class="text-sm text-red-400">{{ $message }}</span>
                                 @enderror
